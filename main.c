e9cdef367c614cefb167e1c43408a7ab9316925b
skandigraun <skandigraun@gmail.com>
2022-11-24 20:27:29 +0100

    commit

--- main.c
+++ main.c
@@ -1,15 +1,48 @@
 #include <stdio.h>
+#include <unistd.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <sys/ioctl.h>
 #include <linux/input.h>
+#include <time.h>
 
 int main(int argc, char *argv[])
 {
     int fd = open("/dev/input/event2", O_RDWR);
-    int n, res;
-    res = ioctl(fd, EVIOCGEFFECTS, &n);
-    printf("ioctl result: %i\n", res);
-    printf("The number of effects that can be stored: %i\n", n);
+
+    struct ff_effect ffe;
+    struct input_event play;
+    struct input_event stop;
+
+    struct timespec hundred_ms;
+    hundred_ms.tv_sec = 0;
+    hundred_ms.tv_nsec = 100000000;
+
+    char c;
+
+    ffe.type = FF_PERIODIC;
+    ffe.id = -1;
+    ffe.u.periodic.custom_len = 500;
+    ffe.u.periodic.waveform = FF_SQUARE;
+
+    ioctl(fd, EVIOCSFF, &ffe);
+
+    play.code = ffe.id;
+    play.type = EV_FF;
+    play.value = 3;
+
+    stop.code = ffe.id;
+    stop.type = EV_FF;
+    stop.value = 0;
+
+
+    while (read(STDIN_FILENO, &c, 1) > 0) {
+        write(fd, (const void*) &play, sizeof(play));
+        nanosleep(&hundred_ms, &hundred_ms);
+        write(fd, (const void*) &stop, sizeof(stop));
+    }
+
+    close(fd);
+
     return 0;
 }
---
+++ readme.md
@@ -0,0 +1,3 @@
+Very simple program that makes the PinePhone's force feedback go `BrRrRrRrRr` for a short period, whenever it reads something from stdin.
+
+I use it with [svkbd](https://tools.suckless.org/x/svkbd/) to add some haptic feedback whenever I press a key.
